6a445bb75801a479a45e1d553f3cd78f63bea57f
mahendra dhamala <rijalmpd1234@gmail.com>
2024-05-25 11:46:44 +0545

    fixed input text color

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -4,7 +4,7 @@ const Popup = ({ setShowPopUp }: any) => {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen bg-slate-700 bg-opacity-50  backdrop-blur-sm flex items-center justify-center">
       <div className="relative bg-white dark:bg-slate-700 p-4 rounded shadow w-[70%] h-[20rem]">
-        <input type="text" className="w-full mt-10 h-8 outline-none p-6 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-slate-700" placeholder="search here..." autoFocus />
+        <input type="text" className="w-full mt-10 h-8 outline-none p-6 border border-gray-300 rounded focus:outline-none focus:ring-2 dark:text-white focus:ring-blue-500 dark:bg-slate-700" placeholder="search here..." autoFocus />
         <IoClose
           className="absolute top-4 right-4 h-6 w-6 cursor-pointer dark:text-white"
           onClick={() => setShowPopUp(false)}
